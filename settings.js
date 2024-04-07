faa89de2b7f3c213e3ab3ac2d196f0d9556950fc
ishan-md2024 <166141204+ishan-md2024@users.noreply.github.com>
2024-04-08 01:49:14 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,10 +18,10 @@ Creator : @darkmakerofc , @mr-nima-x
 module.exports = {
   OWNER_NUMBER: "94715166712",
   SUDO_NUMBERS : "94715166712,94719574492",
-  OWNER_NAME: "『 𝙼𝚁 𝙽𝙸𝙼𝙰 』",
-  BOT_NAME : "ＺＵＳＹＣＯ",
+  OWNER_NAME: "『 ishan 』",
+  BOT_NAME : "ishuea",
   ZUSYCO_FILE : "ZUSYCO-QR",
-  LOGOS: {
+  LOGOS: {ZUSYCO-QR.ishan
     ALIVE: 'https://i.ibb.co/YTWznr9/ZUSYCO-ALIVE-IMAGE-LOWQ.jpg',
     WELCOME: 'https://i.ibb.co/PzhpBV7/WELCOME-IMAGE-LOWQ.jpg',
     GOODBYE: 'https://i.ibb.co/t8hKpcM/GOODBYE-IMAGE-LOWQ.jpg'
